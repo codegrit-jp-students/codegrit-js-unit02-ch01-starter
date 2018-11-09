59733d1f1aad5ddfb7ad831d59c19a4d2a3c152a
Fumi Kuranishi <kuranishi3221@gmail.com>
2018-11-09 08:44:48 +0100

    challenge1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
-import _ from 'lodash';
+        import _ from 'lodash';
 
-function addRandomNum() {
-  const mainEl = document.getElementById('main');
-  let p = document.createElement('p')
-  p.innerHTML = "Random Number: " + _.random(0, 10).toString();
-  document.body.appendChild(p)
-}
+        function addRandomNum() {
+            const mainEl = document.getElementById('main');
+            let p = document.createElement('p');
+            let randomNumber = "Random Number: " + _.random(0, 10).toString();
+            p.innerHTML = randomNumber;
+            document.body.appendChild(p);
+        }
 
-{
-  const button1 = document.getElementById('button1')
-  button1.addEventListener("click", addRandomNum);
-  console.log("ready");
-}
+        {
+            const button1 = document.getElementById('button1');
+            button1.addEventListener("click", addRandomNum);
+            console.log("ready");
+        }
+
+        function recordNumber() {
+            randomNumber = [num1, num2, num3, num4, num5]
+             for (i = 0; i >= randomNumber.length; i++) {
+                 _.sum(randomNumber[i]);
+            }
+        }
+
+        function sumFiveNum() {
+            let p = document.createElement('p');
+            p.innerHTML = "Sum of Recent Five Number: " + recordNumber();
+            document.body.appendChild(p);
+        }
+
+        {
+            const button2 = document.getElementById('button2');
+            button2.addEventListener("click", sumFiveNum);
+        }
+
+        // フォークしたリポジトリ上でNPMスクリプトを呼び出してwebpack-dev-serverを立ち上げましょう。
+        // recordNumberというファンクションを作成して、addRandomNumの結果が最大5つまで保存されるようにしましょう。
+        // ボタンをもう一つ作成して、クリックすると保存されている数字の合計を表示するようにしましょう。 数字の合計には、lodashのファンクションの一つを利用して下さい。
