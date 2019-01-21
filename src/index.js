9fe24f681d48a3f7fbd4abfc21bb4c2987784b1d
yuitamura <yuixtamura@gmail.com>
2019-01-21 09:13:05 +0100

    updated index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,32 @@ function addRandomNum() {
   document.body.appendChild(p)
 }
 
+function recordNumber() {
+  const arr = new Array();
+  for (let i = 0; i < 5; i++) {
+  arr[0] = '1';
+  arr[1] = '2';
+  arr[2] = '3';
+  arr[3] = '4';
+  arr[4] = '5';
+  arr[5] = '6';
+  }
+}
+
+function sumNumbers() {
+  _.sum([1, 2, 3, 4, 5])
+  let p = document.createElement('p')
+  p.innerHTML = "Total: " + _.sum.toString();
+}
+
 {
   const button1 = document.getElementById('button1')
   button1.addEventListener("click", addRandomNum);
+  console.log("ready");
+}
+
+{
+  const button2 = document.getElementById('button2')
+  button2.addEventListener("click", sumNumbers);
   console.log("ready");
 }
