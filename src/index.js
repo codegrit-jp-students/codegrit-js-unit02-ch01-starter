7c753d271f5f05c6a5acd4bff8f3b16b8857c4a7
yuitamura <yuixtamura@gmail.com>
2019-02-11 11:32:51 +0100

    updated function addRandomNum

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ function addRandomNum() {
   let p = document.createElement('p')
   p.innerHTML = "Random Number: " + _.random(0, 10).toString();
   document.body.appendChild(p)
+
+  let numbers = [];
+  numbers = [num1, num2, num3, num4, num5];
+  function randomNum() {
+    while(numbers.length < 5) {
+      numbers.push(_.random(0, 10));
+    }
+  }
 }
 
-function recordNumber() {
+/* function recordNumber() {
   let arr = [];
   arr = [num1, num2, num3, num4, num5]; // ランダムなのでconstで固定しない。new Arrayで配列を作るのはあまりオススメしない。数値が変わることがある。作るとしたら、定数の中に配列を作ること。
   // こちらで決められないので、引数で仮定。変数でsumを宣言する。同じことを何度も書かないように。sumメソッドの中身が引数となる。ここではlodashを使わないが、別の場所で使うための下ごしらえをする。
@@ -16,7 +24,7 @@ function recordNumber() {
     let p = document.createElement('p')
     p.innerHTML = "Total: " + _.sum.toString(); // toString()をinnerHTMLと一緒に使ってOK。今回は計算結果を出すので、innerHTMLだけでは不十分。toStringメソッドが含まれる
   }
-}
+} */
 /* lodashインポートしているので、そのままlodashを使える。 */
 
 {
@@ -30,4 +38,3 @@ function recordNumber() {
   button2.addEventListener("click", recordNumber);
   console.log("ready");
 }
-
